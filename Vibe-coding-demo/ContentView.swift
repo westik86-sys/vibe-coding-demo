61fd8289cdbc67e3c77db826ff7b9f1c166515e3
e <e>
2026-02-13 10:36:37 +0300

    Initial Commit

---
+++ Vibe-coding-demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Vibe-coding-demo
+//
+//  Created by Pavel Korostelev on 13.02.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Vibe-coding-demo/Vibe_coding_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vibe_coding_demoApp.swift
+//  Vibe-coding-demo
+//
+//  Created by Pavel Korostelev on 13.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct Vibe_coding_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
